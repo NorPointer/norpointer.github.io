2669342328245de3a7edceb65850d9c242b75f75
Håkon Anders Strømsodd <haakon8855@gmail.com>
2025-05-07 19:55:30 +0200

    Add shadcn button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import logo from '../logo.svg'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -33,6 +34,7 @@ function App() {
         >
           Learn TanStack
         </a>
+        <Button>Click me!</Button>
       </header>
     </div>
   )
