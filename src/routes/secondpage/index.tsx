e48bc66f101d39459b2156ff4eff2cdc5693b61a
Ole-Magnus Vian Norum <ole99@live.no>
2025-06-25 19:18:13 +0200

    Add poke uri

--- src/routes/secondpage/index.tsx
+++ src/routes/secondpage/index.tsx
@@ -4,6 +4,18 @@ export const Route = createFileRoute('/secondpage/')({
   component: RouteComponent,
 })
 
+const getPokemon = async (pokeNumber: number) => {
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon"
+  let response = await fetch(`${baseUrl}/1`)
+  if (!response.ok)
+  {
+    throw Error("FUCK")
+  }
+  let json = await response.json()
+  console.log(json)
+}
+
 function RouteComponent() {
+  getPokemon(1)
   return <div>Oles page!</div>
 }
